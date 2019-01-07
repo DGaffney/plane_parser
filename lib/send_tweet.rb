e9b191e994f3c5aaecf9283630dbf20a97300eae
Devin Gaffney <itsme@devingaffney.com>
2019-01-07 22:12:30 +0000

    updates

--- lib/send_tweet.rb
+++ lib/send_tweet.rb
@@ -2,14 +2,17 @@
 class Tweeter
   
   def self.run
-    self.identify_deals.each do |plane, savings|
-      f = File.open(plane.id.to_s+".json", "w")
-      f.write({tweet: self.tweet_text(plane, savings), username: SETTINGS["twitter_user"], password: SETTINGS["twitter_password"]}.to_json)
-      f.close
-      puts "python scripts/tweet.py #{plane.id.to_s}.json"
-      `python scripts/tweet.py #{plane.id.to_s}.json`
-      `rm #{plane.id.to_s}.json`
-      `pkill chrome`
+    while true
+      self.identify_deals.each do |plane, savings|
+        f = File.open(plane.id.to_s+".json", "w")
+        f.write({tweet: self.tweet_text(plane, savings), username: SETTINGS["twitter_user"], password: SETTINGS["twitter_password"]}.to_json)
+        f.close
+        puts "python scripts/tweet.py #{plane.id.to_s}.json"
+        `python scripts/tweet.py #{plane.id.to_s}.json`
+        `rm #{plane.id.to_s}.json`
+        `pkill chrome`
+      end
+      sleep(60*60)
     end
   end
   
@@ -21,6 +24,6 @@ class Tweeter
   def self.identify_deals
     plane_ids = RawPlane.where(:delisted.ne => true, :deal_tweeted.ne => true, :last_updated.gte => Time.now-60*60*24*30, category_level: "Single+Engine+Piston", :image_count.gte => 2, :price.ne => 0).collect(&:id)
     predicted_prices = Hash[RawPlaneObservation.where(:raw_plane_id.in => plane_ids).collect{|x| [x.raw_plane_id, x.predict_price]}]
-    plane_ids.collect{|x| [RawPlane.find(x), RawPlane.find(x).price-predicted_prices[x]]}.sort_by(&:last).select{|x| x.last.abs/x.first.price < 0.50 && x.first.price < 45000 && x.last < -1000}
+    plane_ids.select{|x| !predicted_prices[x].nil?}.collect{|x| [RawPlane.find(x), RawPlane.find(x).price-predicted_prices[x]]}.sort_by(&:last).select{|x| x.last.abs/x.first.price < 0.50 && x.first.price < 45000 && x.last < -1000}
   end
 end
