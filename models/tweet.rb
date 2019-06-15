884a3d812a6af75952e53b90f6e5d5c0b5b715c6
Devin Gaffney <itsme@devingaffney.com>
2019-06-14 22:10:56 -0400

    updates

--- models/tweet.rb
+++ models/tweet.rb
@@ -13,6 +13,20 @@ class Tweet
     end
   end
 
+  def self.generate_deal_posts
+    categories = ["Piston+Helicopters", "Turboprop", "Single+Engine+Piston", "Multi+Engine+Piston", "Ultralight", "Rotary+Wing", "Gliders+%7C+Sailplanes"]
+    plane_ids = RawPlane.where(:delisted.ne => true, :deal_tweeted.ne => true, :last_updated.gte => Time.now-60*60*24*30, :category_level.in => categories, :image_count.gte => 2, :price.ne => 0).collect(&:id)
+    predicted_prices = Hash[RawPlaneObservation.where(:raw_plane_id.in => plane_ids).collect{|x| [x.raw_plane_id, x.predict_price]}]
+    deals = plane_ids.collect{|x| [RawPlane.find(x), RawPlane.find(x).price.to_f-predicted_prices[x].to_f]}.sort_by(&:last).select{|x| x.last.abs/x.first.price < 0.50 && x.last.abs/x.first.price > 0.05 && x.first.price < 300000 && x.last < 0}
+    deals.each do |plane, savings|
+      t = Tweet.new(raw_plane_id: plane.id, tweet_type: "sold")
+      loc = plane.location && !plane.location.empty? ? " in #{plane.location}" : ""
+      t.tweet_text = "#{plane.year} #{plane.make.capitalize} #{plane.model.capitalize}#{loc} -- $#{plane.price.to_i.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse}, undervalued by ≈#{((savings.abs/plane.price).round(2)*100).to_i}% (posted #{plane._id.generation_time.strftime("%Y-%m-%d")}): https://trade-a-plane.com#{plane.link}"
+      t.save!
+      t.send_tweet
+    end
+  end
+
   def self.generate_sold_posts
     RawPlane.where(delisted: true, :archived_link.nin => ["", nil], :"latest_certficate_reissue_date.last_certificate_date".ne => nil).select{|x| t = (x.last_updated||x.id.generation_time); g = x.latest_certficate_reissue_date["last_certificate_date"]-t; g > 0 && g < 5*4*7*24*60*60}.each do |plane|
       if Tweet.where(raw_plane_id: plane.id, tweet_type: "sold").first.nil?
@@ -68,6 +82,7 @@ class Tweet
   end
 
   def self.check_for_tweets_to_send
+    Tweet.generate_deal_posts
     Tweet.generate_sold_posts
     Tweet.generate_delisted_posts
     Tweet.generate_budget_posts
