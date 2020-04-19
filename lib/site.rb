cdbb844524a21b19343c5e669149ef34ed82d9b9
Devin Gaffney <itsme@devingaffney.com>
2020-04-19 13:46:34 -0400

    updates

--- lib/site.rb
+++ lib/site.rb
@@ -1,5 +1,5 @@
 class Site < Sinatra::Base
-  get "/get_prediction" do
+  get "/get_prediction.json" do
     if (cp = CachedPrediction.where(listing_id: params[:listing_id]).first)
       cp.hits += 1
       cp.save!
@@ -12,6 +12,13 @@ class Site < Sinatra::Base
         return {listing_id: params[:listing_id], predicted_price: cp.predicted_price}.to_json
       end
     end
-    return {listing_id: params[:listing_id], error: "Plane not in database, sorry!"}
+    return {listing_id: params[:listing_id], error: "Plane not in database, sorry!"}.to_json
+  end
+  
+  get "/get_listing_ids.json" do
+    since_time = Time.parse(params[:since]) rescue nil
+    parse_error = since_time.nil?
+    since_time ||= Time.now-60*60*24*7
+    return {since_time: since_time, used_since_time_default: parse_error, listing_ids: RawPlane.where(:created_at.gte => since_time).distinct(:listing_id)}.to_json
   end
 end
