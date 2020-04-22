e55071da43696a395fa76fc1255cc7444c491856
Devin Gaffney <itsme@devingaffney.com>
2020-04-22 16:35:19 -0400

    updates

--- site/index.js
+++ site/index.js
@@ -47,40 +47,8 @@ app.get('/image/:email_id', (req, res) => {
       res.end(trackImg)
   }
 })
-app.get("/lookup_business.json", function(req, res) {
-    api.lookup_business(req.query.name, req.query.location, function(body){
-        res.send(body)
-    })
-})
-app.post("/similar_businesses.json", function(req, res) {
-    api.similar_businesses(req.body, function(body){
-        res.send(body)
-    })
-})
-app.get("/notifications.json", function(req, res) {
-    api.notifications(req.query.business_id, function(body){
-        res.send(body)
-    })
-})
-app.get("/email_detail.json", function(req, res) {
-    api.email_detail(req.query.email_id, 'json', function(body){
-        res.send(body)
-    })
-})
-app.get("/full_notification_download.json", function(req, res) {
-    api.full_notifications(req.query.email_config_id, 'csv', function(body){
-        res.writeHead(200, {'Content-Type': 'application/force-download','Content-disposition':'attachment; filename=localet_email_'+req.query.email_config_id+'.csv'});
-        res.end(body.result);
-    })
-})
-app.get("/email_detail_download.json", function(req, res) {
-    api.email_detail(req.query.email_id, 'csv', function(body){
-        res.writeHead(200, {'Content-Type': 'application/force-download','Content-disposition':'attachment; filename=localet_email_'+req.query.email_id+'.csv'});
-        res.end(body.result);
-    })
-})
-app.get("/email_config.json", function(req, res) {
-    api.notifications(req.query.email_config_id, function(body){
+app.get("/parse_search_page.json", function(req, res) {
+    api.parse_search_page(req.query.url, function(body){
         res.send(body)
     })
 })
@@ -95,78 +63,15 @@ app.get("/unsubscribe.json", function(req, res){
         res.send(body)
     })
 })
-app.post("/alter_email.json", function(req, res){
-    api.alter_email_config(req.body, function(body){
-        res.send(body)
-    })
-})
-app.get("/manage_alerts/:email_config_id", function(req, res){
-    api.get_email_config(req.params.email_config_id, function(body){
-        context = {
-            email_config_id: body._id["$oid"],
-            primary_events: body.primary_events,
-            attribute_updates: body.attribute_updates,
-            reviews: body.reviews,
-            search_ranks: body.search_ranks,
-            subscribed: !body.subscribed,
-        }
-        res.render("./../www/update_email.html", context)
-    })
-})
 app.get("/about", function(req, res){
     res.render("./../www/about.html", {})
 })
 app.get("/privacy", function(req, res){
     res.render("./../www/privacy.html", {})
 })
-app.get("/full_notifications/:email_config_id", function(req, res){
-    api.full_notifications(req.params.email_config_id, 'html', function(body){
-        context = {
-            email_config_id: req.params.email_config_id,
-            date: body.created_at,
-            primary_events: body.result["Big Events"],
-            attribute_updates: body.result["Page Updates"],
-            reviews: body.result["New Reviews"],
-            search_ranks: body.result['Search Result Rankings'],
-        }
-        res.render("./../www/full_notifications.html", context)
-    })
-})
-app.get("/email_detail/:email_id", function(req, res){
-    api.email_detail(req.params.email_id, 'html', function(body){
-        context = {
-            email_id: req.params.email_id,
-            email_config_id: body.email_config_id["$oid"],
-            date: body.created_at,
-            primary_events: body.result["Big Events"],
-            attribute_updates: body.result["Page Updates"],
-            reviews: body.result["New Reviews"],
-            search_ranks: body.result['Search Result Rankings'],
-        }
-        res.render("./../www/email_detail.html", context)
-    })
-})
-app.get("/start_tracking/:email_config_id", function(req, res){
-    api.start_tracking(req.params.email_config_id, function(body){
-        context = {
-            email_config_id: req.params.email_config_id,
-            business: body.business,
-            tracked_businesses: body.tracked_businesses,
-            email_address: body.email_address,
-            marketing_lead_id: body.marketing_lead_id
-        }
-        res.render("./../www/start_tracking.html", context)
-    })
-})
 app.get("/unsubscribe/:email_config_id", function(req, res){
     api.unsubscribe(req.params.email_config_id, function(body){
         res.render("./../www/unsubscribed.html")
-    })
-})
-app.get("/link", function(req, res){
-    api.link(req.query, function(body){
-        res.redirect(body.link)
-        console.log("successfully done")
     })
 })
 app.use('/', express.static('www'));
